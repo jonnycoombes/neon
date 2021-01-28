58d238a270d1e43d05a9ea2b5050a36c4a4663da
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-01-28 22:53:53 +0000

    Added utility for hostname lookup

---
+++ Neon-Glow-Test/Utilities/Network/ResolutionTests.cs
@@ -0,0 +1,29 @@
+﻿#region
+
+using System;
+using JCS.Neon.Glow.Utilities.Network;
+using Xunit;
+
+#endregion
+
+namespace JCS.Neon.Glow.Test.Utilities.Network
+{
+    /// <summary>
+    ///     Test suite for <see cref="Resolution" />
+    /// </summary>
+    [Trait("Category", "Network")]
+    public class ResolutionTests : TestBase, IDisposable
+    {
+        public void Dispose()
+        {
+        }
+
+        [Fact(DisplayName = "Must be able to resolve the current host name")]
+        [Trait("Category", "General")]
+        public void ResolveHostName()
+        {
+            var hostName = Resolution.GetCurrentHostName();
+            Assert.NotNull(hostName);
+        }
+    }
+}
---
+++ Neon-Glow/Utilities/Network/Resolution.cs
@@ -0,0 +1,55 @@
+﻿#region
+
+using System;
+using System.Net;
+using JCS.Neon.Glow.Utilities.General;
+using Serilog;
+
+#endregion
+
+namespace JCS.Neon.Glow.Utilities.Network
+{
+    /// <summary>
+    ///     Contains a series of static utility/helper methods relating to network resolution
+    /// </summary>
+    public static class Resolution
+    {
+        /// <summary>
+        ///     The environment variable that can be used to look up a hostname
+        /// </summary>
+        public const string HostNameEnvironmentKey = "HOSTNAME";
+
+        /// <summary>
+        ///     Static logger
+        /// </summary>
+        private static readonly ILogger _log = Log.ForContext(typeof(Resolution));
+
+        /// <summary>
+        ///     Attempts to retrieve the current host name.  Will try and do this in a few different ways:
+        ///     1. Dns resolution
+        ///     2. NetBIOS machine name call
+        ///     3. Extraction from a named environment variable - HOSTNAME
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCurrentHostName()
+        {
+            Logs.MethodCall(_log);
+            try
+            {
+                var hostName = Dns.GetHostName();
+                if (hostName == null)
+                {
+                    hostName = Environment.GetEnvironmentVariable(HostNameEnvironmentKey);
+                    if (hostName == null) hostName = Environment.MachineName;
+                }
+
+                return hostName;
+            }
+            catch (Exception ex)
+            {
+                Logs.Warning(_log, "Exception whilst attempting the lookup of current hostname");
+                return Environment.MachineName;
+            }
+        }
+    }
+}
