e19c2c109a742b23b2c992e00c589fabca2606df
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-01-28 22:19:54 +0000

    Added XML documentation

--- Neon-Glow/Utilities/General/Exceptions.cs
+++ Neon-Glow/Utilities/General/Exceptions.cs
@@ -16,6 +16,15 @@ namespace JCS.Neon.Glow.Utilities.General
         /// </summary>
         private static Dictionary<Type, ConstructorInfo> _ctorCache = new();
 
+        /// <summary>
+        /// Generates a new exception of a given type, and logs the supplied message as an
+        /// error first.  An optional inner exception can be supplied
+        /// </summary>
+        /// <param name="log">The <see cref="ILogger"/> instance to use for logging</param>
+        /// <param name="message">The message to be logged, and then set as the message for the generated exception</param>
+        /// <param name="inner">An optional inner exception</param>
+        /// <typeparam name="E">The type of exception to be generated</typeparam>
+        /// <returns>A new instance of type E, derived from <see cref="Exception"/></returns>
         public static E LoggedException<E>(ILogger log, string message, Exception inner = null)
             where E : Exception
         {
