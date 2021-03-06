e4a418c585a736e59e043445e8404c620d6dd845
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-03-06 10:56:01 +0000

    Added overloaded method which doesn't require an inner exception in order to be called

--- Neon-Glow/Exceptions/ExceptionHelper.cs
+++ Neon-Glow/Exceptions/ExceptionHelper.cs
@@ -2,6 +2,7 @@
 
 using System;
 using JCS.Neon.Glow.Logging;
+using JCS.Neon.Glow.Reflection;
 using Serilog;
 
 #endregion
@@ -22,12 +23,27 @@ namespace JCS.Neon.Glow.Exceptions
         /// <param name="message">The message to be logged, and then set as the message for the generated exception</param>
         /// <param name="inner">An optional inner exception</param>
         /// <typeparam name="E">The type of exception to be generated</typeparam>
-        /// <returns>A new instance of type E, derived from <see cref="Exception" /></returns>
+        /// <returns>A new instance of type <see cref="E" />, derived from <see cref="Exception" /></returns>
         public static E LoggedException<E>(ILogger log, string message, Exception inner = null!)
             where E : Exception
         {
             LogHelper.Error(log, message);
-            return (E) Reflection.ReflectionHelper.CreateException<E>(message, inner);
+            return (E) ReflectionHelper.CreateException<E>(message, inner);
+        }
+
+        /// <summary>
+        ///     Generates a new exception of the given type, and logs the supplied message as an error first. No inner exception
+        ///     can be supplied with this variant of the method
+        /// </summary>
+        /// <param name="log">The <see cref="ILogger" /> instance to log to</param>
+        /// <param name="message">The message to be logged</param>
+        /// <typeparam name="E">The type of the exception to be generated and returned</typeparam>
+        /// <returns>A new instance of type <see cref="E" />, derived from <see cref="Exception" /></returns>
+        public static E LoggedException<E>(ILogger log, string message)
+            where E : Exception
+        {
+            LogHelper.Error(log, message);
+            return (E) ReflectionHelper.CreateException<E>(message);
         }
     }
 }
