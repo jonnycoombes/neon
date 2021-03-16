429864484a71fc155896c4128cf1725d36c2792e
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-03-16 10:54:19 +0000

    Added arithmetic helper placeholder class

---
+++ Neon-Glow-Test/DateAndTime/ArithmeticHelperTests.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using Xunit;
+
+#endregion
+
+namespace JCS.Neon.Glow.Test.DateAndTime
+{
+    /// <summary>
+    ///     Tests for date/time arithmetic helper functions
+    /// </summary>
+    [Trait("Category", "DateAndTime")]
+    public class ArithmeticHelperTests : TestBase
+    {
+    }
+}
---
+++ Neon-Glow/DateAndTime/ArithmeticHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace JCS.Neon.Glow.DateAndTime
+{
+    /// <summary>
+    ///     Class for holding static helpers which involve arithmetic performed on <see cref="System.DateTime" /> instances, or Noda specific
+    ///     time/data structures.  Internally, even calculations involving the system (.NET) <see cref="System.DateTime" /> structure will be
+    ///     converted internally to Noda representations in order to take advantage of Noda's improved functionality.
+    /// </summary>
+    public class ArithmeticHelper
+    {
+    }
+}
