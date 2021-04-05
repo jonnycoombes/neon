02451b9613adfa204bc333ee02593f42d0a0ab0c
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-04-05 11:49:23 +0100

    Added weekly interval logic and associated basic unit tests

--- Neon-Glow-Test/Statics/DateTime/DateArithmeticTests.cs
+++ Neon-Glow-Test/Statics/DateTime/DateArithmeticTests.cs
@@ -1,5 +1,8 @@
 ﻿#region
 
+using System;
+using JCS.Neon.Glow.Statics.DateTime;
+using NodaTime;
 using Xunit;
 
 #endregion
@@ -12,5 +15,69 @@ namespace JCS.Neon.Glow.Test.Statics.DateTime
     [Trait("Category", "Dates")]
     public class DateArithmeticTests : TestBase
     {
+        
+        /// <summary>
+        /// Static set of start date fixtures
+        /// </summary>
+        private static LocalDate[] startDates = new LocalDate[]
+        {
+            LocalDate.FromDateTime(System.DateTime.Today)
+        };
+
+        /// <summary>
+        /// Static set of end date fixtures
+        /// </summary>
+        private static LocalDate[] endDates = new LocalDate[]
+        {
+            startDates[0].Plus(Period.FromDays(4)),
+            startDates[0].Plus(Period.FromMonths(2)),
+            startDates[0].Plus(Period.FromDays(1))
+        };
+        
+        
+        [Fact(DisplayName = "Cannot create weekly buckets with incorrect start and end dates")]
+        [Trait("Category", "Dates")]
+        public void IncorrectStartAndEndDates()
+        {
+            var start = LocalDate.FromDateTime(System.DateTime.Now);
+            var end = start.PlusDays(-1);
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var intervals = DateTimeArithmetic.WeeklyIntervals(start, end, IsoDayOfWeek.Sunday);
+            });
+        }
+        
+        [Fact(DisplayName = "Cannot create weekly buckets with incorrect starting day")]
+        [Trait("Category", "Dates")]
+        public void IncorrectStartingDay()
+        {
+            var start = LocalDate.FromDateTime(System.DateTime.Now);
+            var end = start.PlusDays(5);
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var intervals = DateTimeArithmetic.WeeklyIntervals(start, end, IsoDayOfWeek.None);
+            });
+        }
+
+        [Fact(DisplayName = "Should create 2 intervals for a +7 date range")]
+        public void SevenDayBucketCountCheck()
+        {
+            var start = LocalDate.FromDateTime(System.DateTime.Today);
+            var end = start.PlusDays(7);
+            var intervals = DateTimeArithmetic.WeeklyIntervals(start, end, start.DayOfWeek);
+            var count = intervals.Length;
+            Assert.Equal(2, count);
+        }
+            
+        [Fact(DisplayName = "Should create 2 intervals for a +14 date range")]
+        public void FourteenDayBucketCountCheck()
+        {
+            var start = LocalDate.FromDateTime(System.DateTime.Today);
+            var end = start.PlusDays(14);
+            var intervals = DateTimeArithmetic.WeeklyIntervals(start, end, start.DayOfWeek);
+            var count = intervals.Length;
+            Assert.Equal(3, count);
+        }
+        
     }
 }
--- Neon-Glow/Statics/DateTime/DateTimeArithmetic.cs
+++ Neon-Glow/Statics/DateTime/DateTimeArithmetic.cs
@@ -1,6 +1,8 @@
 ﻿#region
 
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using NodaTime;
 
 #endregion
@@ -8,47 +10,117 @@ using NodaTime;
 namespace JCS.Neon.Glow.Statics.DateTime
 {
     /// <summary>
-    ///     Class for holding static helpers which involve arithmetic performed on <see cref="System.DateTime" /> instances, or Noda specific
-    ///     time/data structures.  Internally, even calculations involving the system (.NET) <see cref="System.DateTime" /> structure will be
+    ///     Class for holding static helpers which involve arithmetic performed on <see cref="System.DateTime" /> instances, or
+    ///     Noda specific
+    ///     time/data structures.  Internally, even calculations involving the system (.NET) <see cref="System.DateTime" />
+    ///     structure will be
     ///     converted internally to Noda representations in order to take advantage of Noda's improved functionality.
     /// </summary>
     public static class DateTimeArithmetic
     {
         /// <summary>
+        ///     <see cref="Period" /> representing a single second
         /// </summary>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="startDay"></param>
-        /// <param name="endDay"></param>
-        /// <returns></returns>
-        public static DateInterval[] WeekdayBuckets(System.DateTime start, System.DateTime end, IsoDayOfWeek startDay, IsoDayOfWeek endDay)
-        {
-            return WeekdayBuckets(LocalDateTime.FromDateTime(start), LocalDateTime.FromDateTime(end), startDay, endDay);
-        }   
+        private static Period SingleSecond = Period.FromSeconds(1);
 
-          /// <summary>
+        /// <summary>
+        ///     <see cref="Period" /> representing a single day
         /// </summary>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="startDay"></param>
-        /// <param name="endDay"></param>
-        /// <returns></returns>
-        public static DateInterval[] WeekdayBuckets(LocalDateTime start, LocalDateTime end, IsoDayOfWeek startDay, IsoDayOfWeek endDay)
+        private static readonly Period SingleDay = Period.FromDays(1);
+
+        /// <summary>
+        ///     <see cref="Period" /> representing a single week
+        /// </summary>
+        private static Period SingleWeek = Period.FromWeeks(1);
+
+        /// <summary>
+        ///     <see cref="Period" /> representing a single month
+        /// </summary>
+        private static Period SingleMonth = Period.FromMonths(1);
+
+        /// <summary>
+        ///     <see cref="Period" /> representing a single year
+        /// </summary>
+        private static Period SingleYear = Period.FromYears(1);
+
+        
+        /// <summary>
+        ///     This method will create a list of 7 day date interval buckets starting on a given <see cref="startDay" />, so that
+        ///     the intervals combined are *inclusive* of both the <see cref="start" /> and <see cref="end" /> dates.
+        /// </summary>
+        /// <param name="start">A <see cref="LocalDate" /> the earliest date to include in the buckets combined</param>
+        /// <param name="end">A <see cref="LocalDate" /> the latest date to include in the buckets combined</param>
+        /// <param name="startDay">The <see cref="IsoDayOfWeek" /> to start each bucket on</param>
+        /// <returns>
+        ///     A list of <see cref="DateInterval" /> non-intersecting intervals, which when combined will include both the
+        ///     specified <see cref="start" /> and <see cref="end" /> dates.
+        /// </returns>
+        public static DateInterval[] WeeklyIntervals(System.DateTime start, System.DateTime end, IsoDayOfWeek startDay)
         {
-            return WeekdayBuckets(start.Date, end.Date, startDay, endDay);
+            return WeeklyIntervals(LocalDateTime.FromDateTime(start), LocalDateTime.FromDateTime(end), startDay);
         }
 
         /// <summary>
-        ///  
+        ///     This method will create a list of 7 day date interval buckets starting on a given <see cref="startDay" />, so that
+        ///     the intervals combined are *inclusive* of both the <see cref="start" /> and <see cref="end" /> dates.
         /// </summary>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="startDay"></param>
-        /// <param name="endDay"></param>
-        /// <returns></returns>
-        public static DateInterval[] WeekdayBuckets(LocalDate start, LocalDate end, IsoDayOfWeek startDay, IsoDayOfWeek endDay)
+        /// <param name="start">A <see cref="LocalDate" /> the earliest date to include in the buckets combined</param>
+        /// <param name="end">A <see cref="LocalDate" /> the latest date to include in the buckets combined</param>
+        /// <param name="startDay">The <see cref="IsoDayOfWeek" /> to start each bucket on</param>
+        /// <returns>
+        ///     A list of <see cref="DateInterval" /> non-intersecting intervals, which when combined will include both the
+        ///     specified <see cref="start" /> and <see cref="end" /> dates.
+        /// </returns>
+        public static DateInterval[] WeeklyIntervals(LocalDateTime start, LocalDateTime end, IsoDayOfWeek startDay)
+        {
+            return WeeklyIntervals(start.Date, end.Date, startDay);
+        }
+
+        /// <summary>
+        ///     This method will create a list of 7 day date interval buckets starting on a given <see cref="startDay" />, so that
+        ///     the intervals combined are *inclusive* of both the <see cref="start" /> and <see cref="end" /> dates.
+        /// </summary>
+        /// <param name="start">A <see cref="LocalDate" /> the earliest date to include in the buckets combined</param>
+        /// <param name="end">A <see cref="LocalDate" /> the latest date to include in the buckets combined</param>
+        /// <param name="startDay">The <see cref="IsoDayOfWeek" /> to start each bucket on</param>
+        /// <returns>
+        ///     An array of non-intersecting <see cref="DateInterval" /> instances, which when combined will include both the
+        ///     specified <see cref="start" /> and <see cref="end" /> dates.
+        /// </returns>
+        public static DateInterval[] WeeklyIntervals(LocalDate start, LocalDate end, IsoDayOfWeek startDay)
         {
             var intervals = new List<DateInterval>();
+
+            if (start > end)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start), @"start must be before the end date");
+            }
+
+            if (startDay == IsoDayOfWeek.None)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startDay), @"You must specify a valid starting day for the buckets");
+            }
+
+            var dayPeriod = Period.FromDays(1);
+            var bucketStart = start;
+            while (bucketStart.DayOfWeek != startDay)
+            {
+                bucketStart = bucketStart.Minus(SingleDay);
+            }
+            
+            var done = false;
+            var dateCursor = bucketStart;
+            while (!done)
+            {
+                var interval = new DateInterval(bucketStart, bucketStart.PlusDays(6));
+                intervals.Add(interval);
+                done = interval.Contains(end);
+                if (!done)
+                {
+                    bucketStart = bucketStart.PlusDays(7);
+                }
+            }
+            
             return intervals.ToArray();
         }
     }
