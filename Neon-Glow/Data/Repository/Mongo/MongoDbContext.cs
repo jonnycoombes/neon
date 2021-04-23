95f3921eaa36b88d8d20d2e5972c8d019df11a54
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-04-23 16:55:13 +0100

    Updated version to 0.4.0

--- Neon-Glow/Data/Repository/Mongo/MongoDbContext.cs
+++ Neon-Glow/Data/Repository/Mongo/MongoDbContext.cs
@@ -1,12 +1,22 @@
+#region
+
+using Serilog;
+
+#endregion
+
 namespace JCS.Neon.Glow.Data.Repository.Mongo
 {
     /// <summary>
     ///     Abstract base class for Mongo DB contexts.  Takes care of all the clever stuff relating to lifecycle, session
-    ///     management etc...Derived classes can add automatic support for repository-style access to collections and related functionality
+    ///     management etc...Derived classes can add automatic support for repository-style access to collections and related
+    ///     functionality
     ///     through generic virtual methods and properties.
     /// </summary>
     public abstract class MongoDbContext
     {
-        
+        /// <summary>
+        ///     Static logger
+        /// </summary>
+        private static ILogger _log = Log.ForContext<MongoDbContext>();
     }
 }
