81e02c4f3dad6519dfe5e11b5e3f85692db4b533
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-04-26 15:10:14 +0100

    Added base class for Mongo test suites and fixtures

---
+++ Neon-Glow-Test/Data/Repository/Mongo/MongoTestBase.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JCS.Neon.Glow.Test.Data.Repository.Mongo
+{
+    public abstract class MongoTestBase : TestBase, IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
