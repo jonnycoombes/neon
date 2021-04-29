ce593603273f3a687b0f9365c99f6b81b284fb46
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-04-29 13:06:26 +0100

    Commit

--- Neon-Glow/Data/Repository/Mongo/MongoDbContext.cs
+++ Neon-Glow/Data/Repository/Mongo/MongoDbContext.cs
@@ -10,8 +10,7 @@ namespace JCS.Neon.Glow.Data.Repository.Mongo
 {
     /// <summary>
     ///     Abstract base class for Mongo DB contexts.  Takes care of all the clever stuff relating to lifecycle, session
-    ///     management etc...Derived classes can add automatic support for repository-style access to collections and related
-    ///     functionality
+    ///     management etc...Derived classes can add automatic support for repository-style access to collections and related functionality
     ///     through generic virtual methods and properties.
     /// </summary>
     public abstract class MongoDbContext
