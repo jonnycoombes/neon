4aefe3c65834bf2ed23949afce42ef6dd7b9cc69
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-05-08 09:34:34 +0100

    Added methods for retrieval of type collection and queryable instances

--- Neon-Glow/Data/Repository/Mongo/IMongoDbContext.cs
+++ Neon-Glow/Data/Repository/Mongo/IMongoDbContext.cs
@@ -1,16 +1,6 @@
-/*
-
-    Copyright 2013-2021 © JCS Software Limited
-
-    Author: Jonny Coombes
-
-    Contact: jcoombes@jcs-software.co.uk
-
-    All rights reserved.
-
- */
 #region
 
+using System.Linq;
 using MongoDB.Driver;
 
 #endregion
@@ -36,5 +26,27 @@ namespace JCS.Neon.Glow.Data.Repository.Mongo
         ///     The bound <see cref="IMongoDatabase" /> being used by the context
         /// </summary>
         public IMongoDatabase Database { get; }
+
+        /// <summary>
+        ///     Returns a typed collection interface for interacting with objects of type <see cref="T" />
+        /// </summary>
+        /// <param name="settings">
+        ///     An optional <see cref="MongoCollectionSettings" /> instance.  If not provided, the defaults will
+        ///     be set by the context.
+        /// </param>
+        /// <typeparam name="T">The type of the entities within the collection</typeparam>
+        /// <returns>A <see cref="IMongoCollection{TDocument}" /> instance, bound to an undelrying collection.</returns>
+        public IMongoCollection<T> Collection<T>(MongoCollectionSettings? settings);
+
+        /// <summary>
+        ///     Returns a <see cref="IQueryable{T}" /> interface for interacting with objects of type <see cref="T" />
+        /// </summary>
+        /// <param name="settings">
+        ///     An optional <see cref="MongoCollectionSettings" /> instance.  If not provided, the defaults will
+        ///     be set by the context.
+        /// </param>
+        /// <typeparam name="T">The type of objects stored within the underlying collection</typeparam>
+        /// <returns>A <see cref="IQueryable{T}" /> interface bound to an underlying collection.</returns>
+        public IQueryable<T> Queryable<T>(MongoCollectionSettings? settings);
     }
 }
