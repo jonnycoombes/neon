57d4bb5433c63cfc816df5b6a86fa3039269a71a
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-05-10 08:31:07 +0100

    Changed attribute property name

--- Neon-Glow/Data/Repository/Mongo/Attributes/Collection.cs
+++ Neon-Glow/Data/Repository/Mongo/Attributes/Collection.cs
@@ -20,21 +20,21 @@ namespace JCS.Neon.Glow.Data.Repository.Mongo
     /// <summary>
     ///     An attribute which allows the collection associated with a given BSON model type to be specified
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class Collection : Attribute
     {
         /// <summary>
         ///     The default constructor takes the name of a collection to be associated with the attributed class
         /// </summary>
-        /// <param name="collectionName">The name of the target collection for the attributed class</param>
-        public Collection(string collectionName)
+        /// <param name="name">The name of the target collection for the attributed class</param>
+        public Collection(string name)
         {
-            CollectionName = collectionName;
+            Name = name;
         }
 
         /// <summary>
         ///     The name of the collection to be associated with the attributed class
         /// </summary>
-        public string CollectionName { get; set; }
+        public string Name { get; set; }
     }
 }
